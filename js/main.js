3eb2fb9fc1dde8f862d3d8634e56b6aaefb8e279
Tim Myers <realdenvercoder@gmail.com>
2017-06-20 22:49:42 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+var game = new Phaser.Game(640, 360, Phaser.AUTO);
+
+var GameState = {
+    preload: function() {
+        //Load assets
+
+    },
+
+    create: function() {
+        //Setup Game
+
+    },
+
+    update: function () {
+        //GameLoop that runs ~ 60x per second
+
+    }
+};
